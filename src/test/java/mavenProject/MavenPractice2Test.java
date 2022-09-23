5b4b2cdca196bb872d48647e17dad46bffce5999
Rakshitha <Rakshitha@DESKTOP-3KLB41R>
2022-09-23 11:57:39 +0530

    First commit

---
+++ src/test/java/mavenProject/MavenPractice2Test.java
@@ -0,0 +1,18 @@
+package mavenProject;
+
+import org.testng.annotations.Test;
+
+public class MavenPractice2Test
+{
+	@Test
+	public void step1Test()
+	{
+
+System.out.println("test-1");
+System.out.println("test-2");
+System.out.println("test-3");
+
+		
+
+	}
+}
---
+++ src/test/java/mavenProject/MavenPracticeTest.java
@@ -0,0 +1,12 @@
+package mavenProject;
+
+import org.testng.annotations.Test;
+
+public class MavenPracticeTest {
+
+	@Test
+	public void test1() {
+		System.out.println("Practice 1");
+	}
+	
+}
